6367ba4be1022a17713a8ce00cadc6073969137f
lautaro <lautaroalegre08@gmail.com>
2023-11-08 20:43:07 -0300

    edite funcion get

--- app/controllers/product.api.controller.php
+++ app/controllers/product.api.controller.php
@@ -1,53 +1,73 @@
 <?php
-    require_once 'app/controllers/api.controller.php';
-    require_once 'app/models/product.model.php';
+require_once 'app/controllers/api.controller.php';
+require_once 'app/models/product.model.php';
 
-    class ProductApiController extends ApiController {
-        private $model;
-        
+class ProductApiController extends ApiController
+{
+    private $model;
 
-        function __construct() {
-            parent::__construct();
-            $this->model = new ProductModel();
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->model = new ProductModel();
+    }
+
+    function get(){
+        if (isset($_GET['sort']) && isset($_GET['order'])) {
+            $sort = $_GET['sort'];
+            $order = $_GET['order'];
             
-        }
 
-        function get($paramns = []){
+            if ($order == 'desc') {
+                 $productos = $this->model->sortByParamDesc($sort);
+                 $this->view->response($productos,200);
+
+            } elseif ($order == 'asc') {
+                $productos = $this->model->sortByParamAsc($sort);
+                $this->view->response($productos,200);
+
+            } else {
+
+                $this->view->response('la opciond de ordenar ' . $order . ' no existe.', 404);
+            }
+        } else {
             $productos = $this->model->getProducts();
+
             $this->view->response($productos,200);
         }
-        function delete($params = []) {
-            $id = $params[':ID'];
-            $product = $this->model->getProduct($id);
-
-            if($product) {
-                $this->model->deleteTask($id);
-                $this->view->response('La tarea con id='.$id.' ha sido borrada.', 200);
-            } else {
-                $this->view->response('La tarea con id='.$id.' no existe.', 404);
-            }
-        }
-        function update($params = []) {
-            $id = $params[':ID'];
-            $tarea = $this->model->getProduct($id);
-
-            if($tarea) {
-                $body = $this->getData();
-                $nombre_producto = $body->nombre_producto;
-                $tipo_de_product = $body->tipo_de_product;
-                $precio = $body->precio;
-                $descripcion_product = $body->descripcion_product;
-                $id_marca = $body->id_marca;
-
-                $this->model->updateProduct($nombre_producto,$tipo_de_product,$precio,$descripcion_product,$id_marca,$id);
-
-                $this->view->response('La tarea con id='.$id.' ha sido modificada.', 200);
-            } else {
-                $this->view->response('La tarea con id='.$id.' no existe.', 404);
-            }
-        }
-
-        
-
-        
+       
     }
+    function delete($params = [])
+    {
+        $id = $params[':ID'];
+        $product = $this->model->getProduct($id);
+
+        if ($product) {
+            $this->model->deleteTask($id);
+            $this->view->response('La tarea con id=' . $id . ' ha sido borrada.', 200);
+        } else {
+            $this->view->response('La tarea con id=' . $id . ' no existe.', 404);
+        }
+    }
+    function update($params = [])
+    {
+        $id = $params[':ID'];
+        $tarea = $this->model->getProduct($id);
+
+        if ($tarea) {
+            $body = $this->getData();
+            $nombre_producto = $body->nombre_producto;
+            $tipo_de_product = $body->tipo_de_product;
+            $precio = $body->precio;
+            $descripcion_product = $body->descripcion_product;
+            $id_marca = $body->id_marca;
+
+            $this->model->updateProduct($nombre_producto, $tipo_de_product, $precio, $descripcion_product, $id_marca, $id);
+
+            $this->view->response('La tarea con id=' . $id . ' ha sido modificada.', 200);
+        } else {
+            $this->view->response('La tarea con id=' . $id . ' no existe.', 404);
+        }
+    }
+}
--- app/models/product.model.php
+++ app/models/product.model.php
@@ -10,27 +10,43 @@ class ProductModel  extends Model {
         $query = $this->db->prepare('SELECT * FROM productos');
         $query->execute();
 
-        // $tasks es un arreglo de tareas
+        
         $products = $query->fetchAll(PDO::FETCH_OBJ);
 
         return $products;
     }
+
      function getProduct($id) {
         $query = $this->db->prepare('SELECT * FROM productos WHERE ?');
         $query->execute([$id]);
 
-        // $tasks es un arreglo de tareas
+        
         $product = $query->fetchAll(PDO::FETCH_OBJ);
 
         return $product;
     }
+
     function deleteTask($id) {
         $query = $this->db->prepare('DELETE FROM productos WHERE product_id = ?');
         $query->execute([$id]);
     }
+
     function updateProduct($nameProduct,$typeProduct,$cost,$productDescription,$id_marca,$id){
         $query = $this->db->prepare('UPDATE productos SET  nombre_producto=?, tipo_de_product=?, precio=?,descripcion_product=?, id_marca = ? WHERE product_id = ?');
         $query->execute([$nameProduct,$typeProduct,$cost,$productDescription,$id_marca,$id]);
     }
- 
+
+    function sortByParamDesc($sort) {
+        $query = $this->db->prepare("SELECT * FROM productos ORDER BY $sort DESC");
+        $query->execute();
+        $products = $query->fetchAll(PDO::FETCH_OBJ);
+        return $products;
+    }
+    
+    function sortByParamAsc($sort) {
+        $query = $this->db->prepare("SELECT * FROM productos ORDER BY $sort ASC");
+        $query->execute();
+        $products = $query->fetchAll(PDO::FETCH_OBJ);
+        return $products;
+    }
 }
